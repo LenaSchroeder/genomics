d7845a6cdf71d1f637ed10f58f434ed0053d2286
Lena <lena.schroeder@tum.de>
2020-04-20 16:44:07 +0200

    add test function

--- dna.py
+++ dna.py
@@ -1,12 +1,18 @@
 from typing import NewType
+import pytest
 
 Sequence = NewType("Sequence", str)
 
-
 Sequence: str
+
 
 def calculate_gc_ratio(seq: Sequence) -> float:
     return len(seq) - 1
 
-assert calculate_gc_ratio('GC') == 1, "If there's equal number of Gs and Cs, it should be 1!"
-assert calculate_gc_ratio('GGC') == 2, "If there's equal number of Gs and Cs, it should be 1!"
+
+def text_equal_gcs_is_one():
+    assert calculate_gc_ratio("GC") == 1
+
+
+def test_double_gcs_is_two():
+    assert calculate_gc_ratio("GGC") == 2, "If there's twice as many number of Gs to Cs, it should be 2!"
