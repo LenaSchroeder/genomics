70f37487a1f80c2a740cd5176b31cbd0fcce6d7e
Lena <lena.schroeder@tum.de>
2020-04-20 16:48:47 +0200

    add gitignore file

--- dna.py
+++ dna.py
@@ -3,16 +3,14 @@ import pytest
 
 Sequence = NewType("Sequence", str)
 
-Sequence: str
-
 
 def calculate_gc_ratio(seq: Sequence) -> float:
     return len(seq) - 1
 
 
-def text_equal_gcs_is_one():
+def test_equal_gcs_is_one():
     assert calculate_gc_ratio("GC") == 1
 
 
 def test_double_gcs_is_two():
-    assert calculate_gc_ratio("GGC") == 2, "If there's twice as many number of Gs to Cs, it should be 2!"
+    assert calculate_gc_ratio("GGC") == 2
