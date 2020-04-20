5fcba6b683e87613a5a246f380f5d84bada5efcb
Lena <lena.schroeder@tum.de>
2020-04-20 16:30:31 +0200

    added initial tests

--- dna.py
+++ dna.py
@@ -1,4 +1,12 @@
+from typing import NewType
+
+Sequence = NewType("Sequence", str)
+
+
 Sequence: str
 
 def calculate_gc_ratio(seq: Sequence) -> float:
-    pass
+    return len(seq) - 1
+
+assert calculate_gc_ratio('GC') == 1, "If there's equal number of Gs and Cs, it should be 1!"
+assert calculate_gc_ratio('GGC') == 2, "If there's equal number of Gs and Cs, it should be 1!"
